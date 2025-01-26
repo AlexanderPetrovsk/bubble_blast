65752b861dc53815942fa62c8b2ba3ac73bdfa43
unknown <petrovski.aleksandareeb@gmail.com>
2025-01-26 17:08:36 +0100

    add anims

--- scripts/sun.gd
+++ scripts/sun.gd
@@ -7,11 +7,12 @@ const min_camera_pos = 750
 const max_camera_pos = 5250
 
 func _process(delta: float) -> void:
-	var levelLength = camera_2d.limit_right - camera_2d.limit_left
-	var cameraLength = 300
-	
-	#sun/cam = campos/level
-	var cameraPosition = camera_2d.global_position.x
-	var predictedPos = (cameraPosition/levelLength) * cameraLength + (cameraPosition - 500)
-	if predictedPos > 150 and predictedPos < 5850:
-		position.x = predictedPos
+	pass
+	#var levelLength = camera_2d.limit_right - camera_2d.limit_left
+	#var cameraLength = 300
+	#
+	##sun/cam = campos/level
+	#var cameraPosition = camera_2d.global_position.x
+	#var predictedPos = (cameraPosition/levelLength) * cameraLength + (cameraPosition - 500)
+	#if predictedPos > 150 and predictedPos < 5850:
+		#position.x = predictedPos
