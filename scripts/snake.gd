c7e49a75c0b3ea1382a438b6ac33e4b1e5da43ce
unknown <petrovski.aleksandareeb@gmail.com>
2025-01-26 16:00:37 +0100

    revertt unwanted

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -5,6 +5,8 @@ extends Area2D
 @onready var collision_shape_2d_2: CollisionShape2D = $CollisionShape2D2
 @onready var tree_shake_timer: Timer = $TreeShakeTimer
 
+@onready var snake_sound: AudioStreamPlayer = $"../../../../sounds/snake_sound"
+
 var maxGrowth = 850
 var isAnimationPlaying = false
 
@@ -25,8 +27,9 @@ func _on_animated_sprite_2d_frame_changed() -> void:
 		collision_shape_2d.position.y -= 15
 		collision_shape_2d.transform.y -= Vector2(0, 71.5)
 
-
 func _on_show_body_entered(body: Node2D) -> void:
+	snake_sound.play()
+
 	if get_parent().has_method('play_tree_shake'):
 		get_parent().play_tree_shake()
 		tree_shake_timer.start()
