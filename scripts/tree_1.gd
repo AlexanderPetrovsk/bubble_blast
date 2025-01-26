5e350760fd66c22b08af3e921f72d3c55072b8f9
unknown <petrovski.aleksandareeb@gmail.com>
2025-01-26 10:23:26 +0100

    add tree shake before snake comes out

--- scripts/tree_1.gd
+++ scripts/tree_1.gd
@@ -1,7 +1,11 @@
 extends Area2D
 
+@onready var animated_sprite_2d_2: AnimatedSprite2D = $AnimatedSprite2D2
 
 func _on_body_entered(body: Node2D) -> void:
 	if !body.isInvincible:
 		if body.has_method('die'):
 			body.die()
+
+func play_tree_shake():
+	animated_sprite_2d_2.play('default')
